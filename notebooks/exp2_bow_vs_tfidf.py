daf4dcadc0f7aa6c627dba78001565dd363221d5
Akarsh <akarshkumar2004@gmail.com>
2026-01-10 20:36:42 +0530

    mlflow

--- notebooks/exp2_bow_vs_tfidf.py
+++ notebooks/exp2_bow_vs_tfidf.py
@@ -28,9 +28,9 @@ warnings.filterwarnings("ignore")
 CONFIG = {
     "data_path": "notebooks/data.csv",
     "test_size": 0.2,
-    "mlflow_tracking_uri": "https://dagshub.com/vikashdas770/YT-Capstone-Project.mlflow",
-    "dagshub_repo_owner": "vikashdas770",
-    "dagshub_repo_name": "YT-Capstone-Project",
+    "mlflow_tracking_uri": "https://dagshub.com/Akarsh-Kumar-2004/reimagined-waffle.mlflow",
+    "dagshub_repo_owner": "akarsh-kumar-2004",
+    "dagshub_repo_name": "reimagined-waffle",
     "experiment_name": "Bow vs TfIdf"
 }
 
